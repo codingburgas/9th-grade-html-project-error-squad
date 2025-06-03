efa83fef1fc76c46a058010830dac9a1ac6f28ce
AYAAtanasov23 <AYAAtanasov23@codinguburgas.bg>
2025-06-03 22:04:07 +0300

    Add edited main page

--- Main page/script.js
+++ Main page/script.js
@@ -1,3 +1,10 @@
 function toggleTheme() {
-  document.body.classList.toggle('dark');
-}
+      document.body.classList.toggle('dark');
+      localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+    }
+
+    window.onload = () => {
+      if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark'); 
+      }
+    };
